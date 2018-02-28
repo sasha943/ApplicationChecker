db7e0e21577c8cb2578a7a5000380706fe334328
oleksandr.maslovskyj <oleksandr.maslovskyj@worldapp.com>
2018-02-28 18:48:12 +0200

    some fix

--- src/main/java/com/worldapp/qa/EmailUtil.java
+++ src/main/java/com/worldapp/qa/EmailUtil.java
@@ -14,7 +14,7 @@ public class EmailUtil {
 
     public static void sendEmailToInfoSec(){
         Properties properties = new Properties();
-        properties.put("mail.smtp.starttls.enable", "faslse");
+        properties.put("mail.smtp.starttls.enable", "false");
         properties.put("mail.smtp.auth", "false");
         properties.put("mail.smtp.host", "localhost");
         properties.put("mail.smtp.port", "8080");
@@ -24,8 +24,8 @@ public class EmailUtil {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(emailAddressFrom));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailAddressTo));
-            message.setSubject("Application checked");
-            message.setText("This is a test");
+            message.setSubject("Warning!!!!!! Application checker.");
+            message.setText("Builds that present in app store or play market does not equals expected version");
             Transport.send(message);
         } catch (MessagingException e){
             e.printStackTrace();
--- src/main/java/com/worldapp/qa/parser/ApkFileParser.java
+++ src/main/java/com/worldapp/qa/parser/ApkFileParser.java
@@ -3,9 +3,14 @@ package com.worldapp.qa.parser;
 
 import net.dongliu.apk.parser.ApkFile;
 import net.dongliu.apk.parser.bean.ApkSigner;
+import sun.misc.BASE64Encoder;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.util.List;
 
@@ -31,5 +36,17 @@ public class ApkFileParser {
 
     public List<ApkSigner> getApkSigners() throws IOException, CertificateException {
         return apkFile.getApkSingers();
+    }
+
+    public String getSHA256Hash(File file) throws NoSuchAlgorithmException, IOException {
+        byte[] buffer= new byte[8192];
+        int count;
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file.getAbsolutePath()));
+        while ((count = bis.read(buffer)) > 0) {
+            digest.update(buffer, 0, count);
+        }
+        byte[] hash = digest.digest();
+        return new BASE64Encoder().encode(hash);
     }
 }
